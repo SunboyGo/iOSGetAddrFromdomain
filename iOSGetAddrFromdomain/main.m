31b8ccd7e07182f176da4b58d87a70e8d13adc39
zk <owenismylove@sina.com>
2014-08-29 09:36:51 +0800

    Initial Commit

---
+++ iOSGetAddrFromdomain/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSGetAddrFromdomain
+//
+//  Created by zk on 14-8-29.
+//  Copyright (c) 2014年 zk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
